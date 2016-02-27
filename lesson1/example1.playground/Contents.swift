6127b456082dae6be994475e017cb3ce834bd0d6
Zhu Yuzhou <zhuyuzhou.dev@gmail.com>
2016-02-27 16:45:33 +1100

    Updated
    Signed-off-by: Zhu Yuzhou <zhuyuzhou.dev@gmail.com>

---
+++ lesson1/example1.playground/Contents.swift
@@ -0,0 +1,19 @@
+/**
+    Code Example 1
+    1. Understand the code
+    2. Change name to your name
+    3. Find difference between var and let (Google is your friend)
+    4. Which var can be changed to let? why?
+*/
+let name = "Zack"
+var str = "Hello, my name is "
+var finalString = str + name
+finalString = "I am a cool guy"
+
+
+
+/**
+    Write your own code below which contains three predefined values (Your name, your parent's names)
+    use let instead of var where possible
+    and make a new string that looks like: Hi, my name is XXX, YYY is my father and my mother's name is ZZZ.
+*/
