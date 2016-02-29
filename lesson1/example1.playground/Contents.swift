bf48dde8f67daa72e5b05de102892cfafef596c0
Erin Wang <workspace4erin@gmail.com>
2016-02-29 12:23:13 +1100

    homework Done

--- lesson1/example1.playground/Contents.swift
+++ lesson1/example1.playground/Contents.swift
@@ -5,8 +5,8 @@
     3. Find difference between var and let (Google is your friend)
     4. Which var can be changed to let? why?
 */
-let name = "Zack"
-var str = "Hello, my name is "
+let name = "Erin"
+let str = "Hello, my name is "
 var finalString = str + name
 finalString = "I am a cool guy"
 
@@ -17,8 +17,13 @@ finalString = "I am a cool guy"
     use let instead of var where possible
     and make a new string that looks like: Hi, my name is XXX, YYY is my father and my mother's name is ZZZ.
 */
-
-
+let myName = "Erin"
+let motherName = "Hong"
+let fatherName = "Dean"
+let str1 = "Hi, my name is "
+let str2 = " is my father and my mother's name is "
+finalString = str1 + myName
+finalString = fatherName + str2 + motherName
 
 
 
@@ -31,3 +36,8 @@ finalString = "I am a cool guy"
 /**
     Use your imagination to write some code do something
 */
+let chs = "猫咪"
+let eng = "cat"
+let str3 = " means "
+let str4 = " in English."
+finalString = chs + str3 + eng + str4
