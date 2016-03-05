d3e90bd2cc05c2f0e691d54a7d1dcd580cded5dc
Zhu Yuzhou <zhuyuzhou.dev@gmail.com>
2016-03-06 10:19:30 +1100

    Add lesson 2
    Signed-off-by: Zhu Yuzhou <zhuyuzhou.dev@gmail.com>

---
+++ lesson2/Example2.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+// Exercise 1, very basic calculation, calculate the age
+let birthYear:Int = 1985
+let thisYear:Int = 2016
+
+// start with let age =
+
+
+// Exercise 2
+// Can word Int above be removed? Why?
+// Answer:
+
+
+// Exercise 3
+// Refer to page 61, calculate how many integers a 8bit system can have
+// how about 16bit, 32bit and 64bit? write the code to solve this problem
