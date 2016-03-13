89dd1f6a4855da2c0f0c650ab12fcdf22937cb5d
Zhu Yuzhou <zhuyuzhou.dev@gmail.com>
2016-03-13 12:08:28 +1100

    Update
    Signed-off-by: Zhu Yuzhou <zhuyuzhou.dev@gmail.com>

---
+++ lesson3/Example3.playground/Contents.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+// use typealias to define an Age type that represents age
+
+
+// define a variable with name of your choice to hold your current age
+
+
+// write a simple control flow, that check the above age variable
+// when age is less than or equal to 2, prints Infant
+// when age is less than 10, but greater than 2, prints Kid
+// when age is less than 18, but greater or equal to 10, prints Teenager
+// otherwise prints Adult
+// verify result by changing your age variable
+// try to make it as simple as possible
+
+
+
+// use typealias and tuple to define a Person type which the first element is an Age type,
+// and second element is an String type which holds the name of the person
+
+
+
+// define a veriable using the Person type with name and value of your choice
+
+
+
+// write another copy of control flow, which will check the age of the Person
+// and prints something like Mike is Adult.
+// verify result by changing the Person variable
+
+
+// Use everything you've learnt by now, design a very simple student exam grading system
+// what it does is it checks the score, and grade with A(90+),B(80-89),C(70,79),D(60-69),F(<60)
+// you must at least use, typealias, control flow, tuple
+// You should make the code as human readable as possible
