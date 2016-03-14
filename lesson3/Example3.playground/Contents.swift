24629e048e771e405fb866d46546c6b9d197b1e2
Erin Wang <workspace4erin@gmail.com>
2016-03-15 10:15:46 +1100

    lesson 3 homework update

--- lesson3/Example3.playground/Contents.swift
+++ lesson3/Example3.playground/Contents.swift
@@ -6,7 +6,8 @@ typealias Age = UInt64
 
 
 // define a variable with name of your choice to hold your current age
-var age:Age = 31
+var age:Age = 10
+
 
 // write a simple control flow, that check the above age variable
 // when age is less than or equal to 2, prints Infant
@@ -15,11 +16,11 @@ var age:Age = 31
 // otherwise prints Adult
 // verify result by changing your age variable
 // try to make it as simple as possible
-if 0 < age && age <= 2 {
+if  age <= 2 {
     print("Infant")
-} else if 2 < age && age < 10 {
+} else if age < 10 {
     print("Kid")
-} else if 10 <= age && age < 18 {
+} else if age < 18 {
     print("Teenager")
 } else {
     print("Adult")
@@ -29,26 +30,30 @@ if 0 < age && age <= 2 {
 
 // use typealias and tuple to define a Person type which the first element is an Age type,
 // and second element is an String type which holds the name of the person
+typealias Name = String
 
-
-typealias Person = (String, UInt64)
+typealias Person = (Name, Age)
 
 
 // define a veriable using the Person type with name and value of your choice
-var A:Person = ("Zack", 1)
+var aPerson:Person = ("Zack", 1)
+var (nameOfPerson, ageOfPerson) = aPerson
 
 // write another copy of control flow, which will check the age of the Person
 // and prints something like Mike is Adult.
 // verify result by changing the Person variable
 
-if 0 < (A.1) && (A.1) <= 2 {
-    print((A.0),"is infant.")
-} else if 2 < (A.1) && (A.1) < 10 {
-    print((A.0),"is Kid.")
-} else if 10 <= (A.1) && (A.1) < 18 {
-    print((A.0),"is Teenager.")
+if ageOfPerson <= 2 {
+    print(nameOfPerson,"is infant.")
+    
+} else if ageOfPerson < 10 {
+    print(nameOfPerson,"is Kid.")
+    
+} else if ageOfPerson < 18 {
+    print(nameOfPerson,"is Teenager.")
+    
 } else {
-    print((A.0),"is Adult.")
+    print(nameOfPerson,"is Adult.")
 }
 
 
@@ -56,19 +61,27 @@ if 0 < (A.1) && (A.1) <= 2 {
 // what it does is it checks the score, and grade with A(90+),B(80-89),C(70,79),D(60-69),F(<60)
 // you must at least use, typealias, control flow, tuple
 // You should make the code as human readable as possible
-typealias Result = (String, UInt64)
+typealias Score = UInt64
 
-var student1:Result = ("Anna", 80)
+typealias Result = (Name, Score)
 
-if (student1.1) >= 90 {
-    print((student1.0),"got an A.")
-} else if 80 <= (student1.1) && (student1.1) < 90 {
-    print((student1.0),"got a B.")
-} else if 70 <= (student1.1) && (student1.1) < 80 {
-    print((student1.0),"got a C.")
-} else if 60 <= (student1.1) && (student1.1) < 70 {
-    print((student1.0),"got a D.")
+var student:Result = ("Anna", 60)
+
+var (nameOfStudent, scoreOfStudent) = student
+
+if scoreOfStudent >= 90 {
+    print(nameOfStudent,"got an A.")
+    
+} else if scoreOfStudent >= 80 {
+    print(nameOfStudent,"got a B.")
+    
+} else if scoreOfStudent >= 70 {
+    print(nameOfStudent,"got a C.")
+    
+} else if scoreOfStudent >= 60 {
+    print(nameOfStudent,"got a D.")
+    
 } else {
-    print((student1.0),"got a F.")
+    print(nameOfStudent,"got a F.")
 }
 
