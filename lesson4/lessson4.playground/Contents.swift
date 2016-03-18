1dd40d1d56760199052249a3f91289267ef77e96
Zhu Yuzhou <space.fd@gmail.com>
2016-03-19 09:53:44 +1100

    Update
    Signed-off-by: Zhu Yuzhou <space.fd@gmail.com>

---
+++ lesson4/lessson4.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+//Exercise 1: In how many ways you can fix error of the folllowing code?
+var name:String?
+name = "Erin"
+let output = "My name is " + name
+
+// Exercise 2: In how many ways you can fix error of the following code?
+// can you update the code that it will check whether the value of numberString is an Integer or not, and if it's an int, it will print out as the int value, otherwise it will print out "This is not an integer" message.
+// Please use Optional Binding and print() function
+let numberString = "345"
+let number:Int = Int(numberString)
