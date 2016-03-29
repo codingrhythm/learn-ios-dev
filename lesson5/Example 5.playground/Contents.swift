bb93b09156dfb17cfd0f3689b825e0f345aa6e6f
Zhu Yuzhou <zhuyuzhou.dev@gmail.com>
2016-03-30 08:22:28 +1100

    Updated
    Signed-off-by: Zhu Yuzhou <zhuyuzhou.dev@gmail.com>

---
+++ lesson5/Example 5.playground/Contents.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+// Say we have a list of names of all Australia State, which is 
+// the best collection type to define the list of state names?
+// why?
+
+
+// Please use the answer above to define a list of Australia States
+// Use for in loop to print all state names
+
+
+
+// How can you write code to find if NSW is an Australia State?
+
+
+
+// Find the differet numbers (not in both list) from following two lists
+let listA:Set<Int> = [341, 542, 12, 45, 68, 76, 45, 12, 1, 3]
+let listB:Set<Int> = [76, 12, 341, 3, 56, 88, 100, 125, 321]
+
+// Find common numbers (included in both list)
+
+
+// Can you use code to check if listC is a subset of listA?
+let listC:Set<Int> = [1, 3, 12]
+
+
+
+// Use an Array to define a list of Australia States, and each element 
+// of the array should be a dictionary which contains some information
+// about the state (name, population, captial city)
+
+
+
+
+// Print out each state in following format
+// State: NSW
+// Captial: Sydney
+// Population: 4452456
+
+
+
+
+// Only print out state which it's population is more than 2 million
+
---
+++ lesson6/Example6.playground/Contents.swift
@@ -0,0 +1,48 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+/**
+ 1. Understand and explain the following code
+ */
+
+for i in 1...15 {
+    if i % 2 == 0 {
+        print("even")
+    } else {
+        print("odd")
+    }
+}
+
+
+/**
+ 2. find and print the index of zeros (0) in the given array
+ */
+
+let digits = [
+    0, 0, 1, 0, 1, 0, 0, 1, 1, 0,
+    0, 1, 0, 1, 1, 1, 0, 0, 0, 1
+]
+
+// you code here
+
+
+/**
+ 3. reverse all digits in digits array, 1->0, 0->1 and put the result in a new array
+ */
+
+
+
+/**
+ 4. make an array which contains 100 zeros
+ */
+
+
+/**
+ 5. write a loop which will change all digits of above array to 1 when the position of that digit can be divided by 2, please note, the position of first digit is 1, although it's index is 0
+ */
+
+
+/**
+ 6. make a new array like step 4. write a loop will count from 1 to the length of the array, and reverse the digit (1->0, 0->1) when the position of that digit can be divided by the count, after the loop, count how many 1s in the array?
+ */
